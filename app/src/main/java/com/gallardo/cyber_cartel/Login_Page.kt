49e6c60a9eb577e7b4acb89c6e9ccaadcc449d0e
Rhev <145690423+RhevMndz@users.noreply.github.com>
2024-02-09 00:54:06 +0800

    pangalawa

---
+++ app/src/main/java/com/gallardo/cyber_cartel/Login_Page.kt
@@ -0,0 +1,40 @@
+package com.gallardo.cyber_cartel
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Login_Page : AppCompatActivity() {
+
+   private lateinit var createAccount: TextView
+   private lateinit var button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+
+        createAccount =findViewById(R.id.tv_createAcc)
+        button = findViewById(R.id.bt_logIn)
+
+        createAccount.setOnClickListener{
+            val intent = Intent(this, Create_account::class.java)
+            startActivity(intent)
+        }
+
+        button.setOnClickListener{
+            Toast.makeText(this, "okay", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+//
+//        createAccount.setOnClickListener {
+//            val intent = Intent(this, Create_account::class.java)
+//            startActivity(intent)
+////            finish()
+//        }
+    }
+}
