ffda1cae45b654fa383c8e0395c82a36cd575508
Rhev <145690423+RhevMndz@users.noreply.github.com>
2024-02-09 22:34:26 +0800

    3rd commit

--- app/src/main/java/com/gallardo/cyber_cartel/Create_account.kt
+++ app/src/main/java/com/gallardo/cyber_cartel/Create_account.kt
@@ -1,11 +1,30 @@
 package com.gallardo.cyber_cartel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Create_account : AppCompatActivity() {
+
+    private lateinit var alreadyhaveanaccount: TextView
+    private lateinit var login: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        alreadyhaveanaccount = findViewById(R.id.tv_alreadyHave_anAccount)
+        login = findViewById(R.id.tv_already_have_an_account_logIn)
+
+        alreadyhaveanaccount.setOnClickListener (){
+            val intent = Intent(this, Login_Page::class.java)
+            startActivity(intent)
+        }
+
+        login.setOnClickListener (){
+            val intent = Intent(this, Login_Page::class.java)
+            startActivity(intent)
+        }
     }
 }
