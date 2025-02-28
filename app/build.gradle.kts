90e4e50c447f7a646af37b168445bb8f12863c3e
PransLoren <frdo.opu-an.up@phinmaed.com>
2025-03-01 00:47:49 +0800

    Initial commit for my new repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
